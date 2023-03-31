f7cdfa134783d03ee70300d7da624cc658f2fed5
Zachary Foutz <zachary@boomlearning.com>
2023-03-31 11:55:26 -0700

    * added capacitor and coughdrop

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zafuzi.disastro',
+  appName: 'disastro',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+export default config;
